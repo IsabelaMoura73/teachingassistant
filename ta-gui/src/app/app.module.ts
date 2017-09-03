42d97dc9c557e0d66b4cc6474b2387b3fdff2339
pauloborba <phmb@cin.ufpe.br>
2017-09-03 18:39:08 -0300

    serviço de aluno injetado, não criado pelo componente

---
+++ ta-gui/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AlunoService } from './aluno.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [AlunoService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
